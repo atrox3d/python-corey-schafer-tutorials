d927c7fdfec2c25f1926a512f80e928a6ce8544a
atrox3d <atrox3d@gmail.com>
2019-11-13 14:42:14 +0100

    add import, rename module file

--- 9-imports.py
+++ 9-imports.py
@@ -2,3 +2,6 @@
 # https://www.youtube.com/watch?v=CqvZ3vGoGs0
 #
 #################################################################################
+import module
+
+courses = ['history', 'math', 'physics', 'compsci']
