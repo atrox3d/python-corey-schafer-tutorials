5f404cc38e501cd521f112e344e008e1ce76256a
atrox3d <atrox3d@gmail.com>
2019-11-13 10:36:40 +0100

    add 6-booleans: equality vs identity

---
+++ 6-booleans.py
@@ -0,0 +1,19 @@
+#
+# https://www.youtube.com/watch?v=DZwmZ8Usvnk
+#
+#################################################################################
+
+# equality and identity
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(f'a = {a}')
+print(f'b = {b}')
+
+print(f'equality: a == b : {a == b}')
+print(f'identity: a is b : {a is b}')
+print(f'id(a): {id(a)}')
+print(f'id(b): {id(b)}')
+
+
