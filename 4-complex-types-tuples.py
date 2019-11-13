8ca4d4a22a4cb48f9b1ed489f305e9bb49e56a8a
atrox3d <atrox3d@gmail.com>
2019-11-13 09:02:21 +0100

    add tuples with assignment exception

--- 4-complex-types-tuples.py
+++ 4-complex-types-tuples.py
@@ -2,8 +2,7 @@
 # https://www.youtube.com/watch?v=W8KRzm-HUcc
 #
 #################################################################################
-# tuples
-
+# lists
 """
  list2 and list1 are the same list, so, if we modify one of them we get both modified
 """
@@ -17,4 +16,21 @@ list1[0] = 'art'
 
 print(list1)
 print(list2)
+
+# tuples
+"""
+"""
+tuple1 = ('history', 'math', 'physics', 'compsci')
+tuple2 = tuple1
+
+print(tuple1)
+print(tuple2)
+
+try:
+    tuple1[0] = 'art'
+except TypeError as te:
+    print(f'Exception: {te}')
+
+print(tuple1)
+print(tuple2)
 
