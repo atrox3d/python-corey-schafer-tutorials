6d595e19b1eb179bfae47f0d166e177a2f89fd8c
atrox3d <atrox3d@gmail.com>
2019-11-13 11:41:54 +0100

    add 7-loops:for

---
+++ 7-loops.py
@@ -0,0 +1,31 @@
+#
+# https://www.youtube.com/watch?v=6iF8Xb7Z3wQ
+#
+#################################################################################
+
+nums = list(range(1, 6))
+print(nums)
+
+print('\nloop with break')
+for num in nums:
+    if num == 3:
+        print('found, break')
+        break
+    print(num)
+
+print('\nloop with continue')
+for num in nums:
+    if num == 3:
+        print('found, continue')
+        continue
+    print(num)
+
+print('\ninner loop')
+for num in nums:
+    for letter in 'abc':
+        print(num, letter)
+
+print('\nrange loop')
+for num in range(10):
+    print(num)
+
