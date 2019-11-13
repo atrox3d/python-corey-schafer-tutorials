429a2543f50aec81747984ba3ec787bcefda73ab
atrox3d <atrox3d@gmail.com>
2019-11-13 09:07:34 +0100

    add print statements

--- 4-complex-types-sets.py
+++ 4-complex-types-sets.py
@@ -9,14 +9,13 @@
 list1 = ['history', 'math', 'physics', 'compsci']
 list2 = list1
 
-print(f'list1: {list1}')
-print(f'list2: {list2}')
+print(list1)
+print(list2)
 
-print("list1[0] = 'art'")
 list1[0] = 'art'
 
-print(f'list1: {list1}')
-print(f'list2: {list2}')
+print(list1)
+print(list2)
 
 # tuples
 """
@@ -24,15 +23,14 @@ print(f'list2: {list2}')
 tuple1 = ('history', 'math', 'physics', 'compsci')
 tuple2 = tuple1
 
-print(f'tuple1: {tuple1}')
-print(f'tuple2: {tuple2}')
+print(tuple1)
+print(tuple2)
 
 try:
-    print("tuple1[0] = 'art'")
     tuple1[0] = 'art'
 except TypeError as te:
     print(f'Exception: {te}')
 
-print(f'tuple1: {tuple1}')
-print(f'tuple2: {tuple2}')
+print(tuple1)
+print(tuple2)
 
