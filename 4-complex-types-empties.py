e80176ae7f404a5ca10c762f22546193c922586d
atrox3d <atrox3d@gmail.com>
2019-11-13 09:25:47 +0100

    add empty declarations

---
+++ 4-complex-types-empties.py
@@ -0,0 +1,17 @@
+#
+# https://www.youtube.com/watch?v=W8KRzm-HUcc
+#
+#################################################################################
+
+# empty lists
+el = []
+el = list()
+
+# empty tuples
+et = ()
+et = tuple()
+
+# empty sets
+es = {} # no, this is an empty dictionary
+es = set()
+
