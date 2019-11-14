c42fb3ede5dee2fc1c8941f58565e15d9e483e8a
atrox3d <atrox3d@gmail.com>
2019-11-14 08:53:00 +0100

    add xkcd example

--- 9-antigravity.py
+++ 9-antigravity.py
@@ -2,3 +2,6 @@
 # https://www.youtube.com/watch?v=CqvZ3vGoGs0
 #
 #################################################################################
+import webbrowser
+
+webbrowser.open('https://xkcd.com/353/')
