08b999ae198d2e097a907435cb05441f98866263
atrox3d <atrox3d@gmail.com>
2019-11-14 14:38:27 +0100

    add 10-LEGB

---
+++ 10-LEGB.py
@@ -0,0 +1,8 @@
+#
+# https://www.youtube.com/watch?v=QVdf0LgmICw
+#
+#################################################################################
+"""
+LEGB
+local, enclosing, global, builtin
+"""
