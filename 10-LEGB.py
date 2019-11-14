d9f7cc7654a43c5be34172f5f1068c29817c460b
atrox3d <atrox3d@gmail.com>
2019-11-14 14:50:51 +0100

    demonstrate scope

--- 10-LEGB.py
+++ 10-LEGB.py
@@ -6,3 +6,38 @@
 LEGB
 local, enclosing, global, builtin
 """
+
+x = 'global x'
+
+
+def printx(x, context='global'):
+    print(f'{context} - x: {x}')
+
+
+def globalx():
+    printx(x, 'function globalx')
+
+
+def localx():
+    # comment or decomment to change x scope
+    # x = 'local x'
+    printx(x, 'function localx')
+
+
+def enclosingx():
+    # comment or decomment to change x scope
+    # x = 'enclosing x'
+    printx(x, 'enclosingx')
+
+    def enclosedx():
+        # comment or decomment to change x scope
+        # x = 'enclosed x'
+        printx(x, 'enclosedx')
+
+    enclosedx()
+
+
+globalx()
+localx()
+enclosingx()
+printx(x)
