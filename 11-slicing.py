b3f11ec48612841bdd48c3d27c99e17a9f261788
atrox3d <atrox3d@gmail.com>
2019-11-15 09:58:54 +0100

    display slicing step two

--- 11-slicing.py
+++ 11-slicing.py
@@ -8,32 +8,42 @@ my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 #        -10,-9,-8,-7,-6,-5,-4,-3,-2,-1
 # list[start:end:stop]
 
+# create indexes list to iterate
 positive_indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 negative_indexes = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
 negative_indexes.reverse()
 
+# show positive indexes formatted
 for p in positive_indexes:
     print(f'{p:>3}', end='')
 print()
 
+# show negative indexes formatted
 for n in negative_indexes:
     print(f'{n:>3}', end='')
 print()
-
+#################################################################################
+print('#' * 80)
+#################################################################################
+#
+#   loop over the two list of indexes simultaneously
+#
 for start, end in zip(positive_indexes, negative_indexes):
-    #print(f'{start:>3}:{end:>3}')
-    if my_list[start:end]:
-        print(f'[{start:>3}:{end:>3}]{" " * start}{my_list[start:end]}')
-    else:
-        print(f'[{start:>3}:{end:>3}]')
-
-# for x in range(0, len(my_list)):
-#     print(f'my_list[{x}] : {my_list[x]}')
-
-# for x in range(0, len(my_list)):
-#     for l in range(0, len(my_list)):
-#         print(f'my_list[{x}:{l}] : {my_list[x:l]}')
-
-# for x in range(0, len(my_list)):
-#     print(f'my_list[x,y] : {my_list}')
-#     print(f'my_list[{x}:{x+3}] : {my_list[x:x+3]}')
+    #
+    # always show the range
+    #
+    print(f'[{start:>3}:{end:>3}]', end='')
+    #
+    # if the range contains data
+    #
+    if len(my_list[start:end]):
+        #
+        # print offset in spaces
+        #
+        print(f'{"   " * start}', end='')
+        #
+        #   print all the matching elements formatted
+        #
+        for x in my_list[start:end]:
+            print(f'{x:>3}', end='')
+    print()
