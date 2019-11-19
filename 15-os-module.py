a674565b3fd94a22235c8ae9ad4f90151b3eaab3
atrox3d <atrox3d@gmail.com>
2019-11-19 08:31:55 +0100

    os.chdir

--- 15-os-module.py
+++ 15-os-module.py
@@ -2,3 +2,25 @@
 #   https://www.youtube.com/watch?v=tJxcKyFMTGo
 #
 #################################################################################
+import os
+
+
+def hashline(width=80):
+    print("#" * width)
+
+
+hashline(160)
+print(dir(os))
+hashline(160)
+
+# where are we
+print('current dir: ', os.getcwd())
+# expand windows %userprofile% variable
+homedirectory = os.path.expandvars('%userprofile%')
+# add desktop
+os.chdir(homedirectory + '\\desktop')
+print('current dir: ', os.getcwd())
+
+
+
+
