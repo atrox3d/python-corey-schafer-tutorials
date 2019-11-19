2c1226a5cc7e25505c49f28880a685d8d76b7a91
atrox3d <atrox3d@gmail.com>
2019-11-19 08:58:58 +0100

    os.makedirs

--- 15-os-module.py
+++ 15-os-module.py
@@ -5,8 +5,8 @@
 import os
 
 
-def hashline(width=80):
-    print("#" * width)
+def hashline(width=80, char='#'):
+    print(char * width)
 
 
 hashline(160)
@@ -19,9 +19,9 @@ change directory
 """
 # where are we
 print('current dir: ', os.getcwd())
-# get user homedirectory, system independent
+# get user homedirectory, os independent
 homedirectory = os.path.expanduser('~')
-# add desktop to home, system independent
+# add desktop to home, os independent
 desktop = os.path.join(homedirectory, 'desktop')
 os.chdir(desktop)
 print('current dir: ', os.getcwd())
@@ -37,4 +37,20 @@ print(os.listdir())
 create directories
 
 """
+try:
+    # create os independent path
+    test_sub = os.path.join('test','sub')
+    # try to create path, will fail.
+    os.mkdir(test_sub)
+except FileNotFoundError as fnfe:
+    print('os.mkdir doesnt create multiple levels:', fnfe)
+except FileExistsError as fee:
+    # silent fail if test exists already
+    pass
+
+# create multiple level path, ignore existing
+os.makedirs(test_sub, exist_ok=True)
+hashline(160)
+print(os.listdir())
+
 
