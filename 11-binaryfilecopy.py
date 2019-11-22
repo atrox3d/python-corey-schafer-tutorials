955b9101436bc022ecacdbc280a60917018613ce
atrox3d <atrox3d@gmail.com>
2019-11-22 12:19:56 +0100

    add binary copy by chunk

--- 11-binaryfilecopy.py
+++ 11-binaryfilecopy.py
@@ -25,9 +25,9 @@ except UnicodeDecodeError as ude:
     print(f'cannot read binary file : {ude}')
 
 """
-try to copy binary file in binary mode
+try to copy binary file in binary mode  line by line
 """
-utils.banner('try to copy binary file in binary mode')
+utils.banner('try to copy binary file in binary mode line by line')
 try:
     #
     #   open source file for reading
@@ -41,6 +41,26 @@ try:
                 wf.write(line)
 except UnicodeDecodeError as ude:
     print(f'cannot read binary file : {ude}')
+
+"""
+try to copy binary file in binary mode  chunk by chunk
+"""
+utils.banner('try to copy binary file in binary mode chunk by chunk')
+#
+#   open source file for reading
+#
+with open('bronx.jpg', 'rb') as rf:
+    #
+    #   open dest file for writing
+    #
+    with open('bronx.copy.chunk.jpg', 'wb') as wf:
+        chunk_size = 4096
+        rf_chunk = rf.read(chunk_size)
+        while len(rf_chunk) > 0:
+            wf.write(rf_chunk)
+            rf_chunk = rf.read(chunk_size)
+
+
 
 
 
