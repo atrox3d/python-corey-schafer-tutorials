174bcb9e87e39a12b4dbc89d9468162b414b188f
atrox3d <atrox3d@gmail.com>
2019-11-22 08:42:13 +0100

    try to write readonly file pointer

--- 11-files-write.py
+++ 11-files-write.py
@@ -3,4 +3,15 @@
 #
 #################################################################################
 import utils
+import io
+
+"""
+try to write on an open for reading file
+"""
+utils.banner('try to write on an open for reading file')
+try:
+    with open('test.txt', 'r') as f:
+        f.write('cannot write file open for read')
+except io.UnsupportedOperation as iuo:
+    print(f'couldnt write to {f.name}: {iuo}')
 
