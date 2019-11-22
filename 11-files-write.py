9409410219ab78ce5345804f9991360bc28ac728
atrox3d <atrox3d@gmail.com>
2019-11-22 11:32:06 +0100

    file write examples

--- 11-files-write.py
+++ 11-files-write.py
@@ -4,6 +4,7 @@
 #################################################################################
 import utils
 import io
+import datetime
 
 """
 try to write on an open for reading file
@@ -15,3 +16,55 @@ try:
 except io.UnsupportedOperation as iuo:
     print(f'couldnt write to {f.name}: {iuo}')
 
+"""
+create new file for writing
+"""
+utils.banner('create new file for writing')
+#
+#   creates file without writing anything
+#
+with open('test2.txt', 'w') as f:
+    pass
+
+with open('test2.txt', 'w') as f:
+    stardate = datetime.datetime.now().strftime("[%d/%m/%y %H:%M]")
+    text = f'{stardate} - write to file test'
+    # print(text)
+    f.write(text)
+
+    #
+    #   file is not yet updated
+    #   doesnt read anything
+    #
+    with open('test2.txt', 'r') as rf:
+        checktext = rf.read()
+        print(checktext)
+
+    # appends the same string
+    f.write(text)
+
+with open('test2.txt', 'r') as rf:
+    checktext = rf.read()
+    print(checktext)
+
+utils.banner('overwrite using seek')
+"""
+write using seek
+"""
+with open('test2.txt', 'w') as f:
+    stardate = datetime.datetime.now().strftime("[%d/%m/%y %H:%M]")
+    text = f'{stardate} - write to file test'
+    # print(text)
+    f.write(text)
+
+    # overwrites the same string
+    f.seek(0)
+    f.write(text)
+
+    # overwrites just one char
+    f.seek(0)
+    f.write('*')
+
+with open('test2.txt', 'r') as rf:
+    checktext = rf.read()
+    print(checktext)
