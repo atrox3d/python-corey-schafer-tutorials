86ade9a0edea23f9663e02d275a61ea952b4ea2a
atrox3d <atrox3d@gmail.com>
2019-11-25 09:35:47 +0100

    update empty types memo

--- 4-complex-types-empties.py
+++ 4-complex-types-empties.py
@@ -16,6 +16,44 @@ ed = {}
 ed = dict()
 
 # empty sets
-es = {} # no, this is an empty dictionary
+es = {}  # no, this is an empty dictionary
 es = set()
 
+#
+# empty generators
+eg = (x for x in [])
+
+#
+# memo
+#
+##############################################################################
+#
+#   empty dictionary to hold example types and description
+#
+type_empties = {}
+#
+#   empty list
+#
+type_empties[type(el)] = el
+#
+#   empty tuple
+#
+type_empties[type(et)] = et
+#
+#   empty dictionary
+#
+type_empties[type(ed)] = ed
+#
+#   empty set
+#
+type_empties[type(es)] = es
+#
+#   empty generator
+#
+type_empties[type(eg)] = eg
+#
+#   display dictionary
+#
+for typeclass, typeempty in type_empties.items():
+    typename = typeclass.__name__
+    print(f'{typename:<10}: {typeempty}')
