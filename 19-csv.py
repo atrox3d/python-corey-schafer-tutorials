70430d2453bd01646dce91dce161238640868b33
atrox3d <atrox3d@gmail.com>
2019-11-27 09:45:08 +0100

    open and dump csv

--- 19-csv.py
+++ 19-csv.py
@@ -6,3 +6,19 @@ import utils
 import csv
 import os
 
+utils.banner('open data/names.csv and list its content')
+csvpath = os.path.join(os.getcwd(), 'data', 'names.csv')
+print(csvpath)
+utils.hashline(char='-')
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+
+    print(csvreader)
+    # skip firt line of generator
+    next(csvreader)
+
+    for line in csvreader:
+        print(line)
+
+
+
