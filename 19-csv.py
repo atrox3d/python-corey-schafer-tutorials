2f839097d0cdc79e9b5df0aff6cbec5c7d3fa1ab
atrox3d <atrox3d@gmail.com>
2019-11-27 11:12:39 +0100

    add csv dictreader

--- 19-csv.py
+++ 19-csv.py
@@ -6,6 +6,15 @@ import utils
 import csv
 import os
 
+#################################################################################
+#
+#
+#
+#   STANDARD CSV READER
+#
+#
+#
+#################################################################################
 utils.banner('open data/names.csv and list its content')
 csvpath = os.path.join(os.getcwd(), 'data', 'names.csv')
 print(csvpath)
@@ -30,10 +39,46 @@ with open(csvpath, 'r') as csvfile:
         for line in csvreader:
             csvwriter.writerow(line)
 
-
 with open(newcsvpath, 'r') as newcsvfile:
     newcsvreader = csv.reader(newcsvfile, delimiter='\t')
     for line in newcsvreader:
         print(line)
 
+#################################################################################
+#
+#
+#
+#   DICTIONARY CSV READER
+#
+#
+#
+#################################################################################
+utils.banner('read csv with dict reader')
+with open(csvpath, 'r') as csvfile:
+    dictreader = csv.DictReader(csvfile)
 
+    for line in dictreader:
+        print('dictionary line: ', line)
+        print('dictionary field: ', line.get('email'))
+
+utils.banner('write csv with dict writer')
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    fieldnames = next(csvreader)
+    print('field names:', fieldnames)
+
+with open(csvpath, 'r') as csvfile:
+    dictreader = csv.DictReader(csvfile)
+
+    with open(newcsvpath, 'w', newline='') as newcsvfile:
+        dictwriter = csv.DictWriter(newcsvfile, fieldnames, delimiter='\t')
+        for line in dictreader:
+            dictwriter.writerow(line)
+
+utils.banner('read new csv with dict reader')
+with open(newcsvpath, 'r') as newcsvfile:
+    dictreader = csv.DictReader(newcsvfile)
+
+    for line in dictreader:
+        print('dictionary line: ', line)
+        print('dictionary field: ', line.get('email'))
