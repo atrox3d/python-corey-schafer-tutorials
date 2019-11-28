91365c8585e76fc66741e35efb1ecdc182f46e22
atrox3d <atrox3d@gmail.com>
2019-11-28 11:19:24 +0100

    explain raw strings

--- 21-re.py
+++ 21-re.py
@@ -2,6 +2,7 @@
 #   https://www.youtube.com/watch?v=K8L6KVGG-7o
 #
 #################################################################################
+import utils
 import re
 
 text_to_search = '''
@@ -25,3 +26,16 @@ Mr. T
 '''
 
 sentence = 'Start a sentence and then bring it to an end'
+
+###############################################################################################
+#
+#   understanding raw strings
+#
+###############################################################################################
+utils.banner('understanding raw strings')
+print('normal strings: the \\t at the beginnig is interpreted into a tab char')
+print('\tTab')
+
+print('raw strings: prepending an r to the quotes r\'\' the \\t at the beginnig is NOT interpreted into a tab char')
+print(r'\tTab')
+
