3bcee647a13531588ba5dd02570ad510a3e2bb8e
atrox3d <atrox3d@gmail.com>
2019-12-04 08:56:13 +0100

    email re example

--- 21-re-emails.py
+++ 21-re-emails.py
@@ -8,5 +8,18 @@ from collections import namedtuple
 import os
 
 
+emails = '''
+CoreyMSchafer@gmail.com
+corey.schafer@university.edu
+corey-321-schafer@my-work.net
+'''
+myemailre = r'[a-zA-Z0-9.-]+@[a-zA-Z-]+\.(com|edu|net)'
+otheremailre = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
 
+pattern = re.compile(otheremailre)
+
+matches = pattern.finditer(emails)
+
+for match in matches:
+    print(match)
 
