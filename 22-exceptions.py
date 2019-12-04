7d8aef1be141c7761eb1cd0159814fbd59c2883f
atrox3d <atrox3d@gmail.com>
2019-12-04 13:35:44 +0100

    basic exception handling

--- 22-exceptions.py
+++ 22-exceptions.py
@@ -3,3 +3,14 @@
 #
 #################################################################################
 import utils
+import os
+
+datafilepath = os.path.join(os.getcwd(), 'data', 'test_file.txt')
+wrongdatafilepath = os.path.join(os.getcwd(), 'data', 'testfile.txt')
+
+try:
+    f = open(wrongdatafilepath)
+except FileNotFoundError as e:
+    print(e)
+except Exception as e:
+    print(e)
