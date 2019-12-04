98a65f3e9518d897611c0367f52af700d19fbfef
atrox3d <atrox3d@gmail.com>
2019-12-04 13:50:35 +0100

    raise

--- 22-exceptions.py
+++ 22-exceptions.py
@@ -5,11 +5,16 @@
 import utils
 import os
 
+corruptfilepath = os.path.join(os.getcwd(), 'data', 'corrupt_file.txt')
 datafilepath = os.path.join(os.getcwd(), 'data', 'test_file.txt')
 wrongdatafilepath = os.path.join(os.getcwd(), 'data', 'testfile.txt')
 
 try:
-    f = open(datafilepath)
+    f = open(corruptfilepath)
+    name = os.path.basename(f.name)
+    if name == 'corrupt_file.txt':
+        print('raising')
+        raise Exception
 except FileNotFoundError as e:
     """
     more specific first
@@ -19,7 +24,7 @@ except Exception as e:
     """
     less specific after
     """
-    print(e)
+    print('error')
 else:
     """
     everything went ok
