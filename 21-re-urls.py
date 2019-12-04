0e2fd1937cd320884ddcf14ba26a4fbcaf43413e
atrox3d <atrox3d@gmail.com>
2019-12-04 10:34:31 +0100

    add comments

--- 21-re-urls.py
+++ 21-re-urls.py
@@ -25,14 +25,27 @@ topleveldomain = r'https?://(www\.)?(\w+)(\.\w+)'
 pattern = re.compile(topleveldomain)
 
 matches = pattern.finditer(urls)
-
+#
+# loop over matches
+#
 for match in matches:
+    #
+    #   loop over groups inside every match
+    #
     groups = []
     for group in match.groups():
+        #
+        # format every group
+        #
         group = "" if group is None else group
         groups.append(f'{group:<10}')
-
+    #
+    #   add the entire match : group 0
+    #
     line = f'{match.group(0):<25}, '
+    #
+    #   join formatted groups
+    #
     line += ", ".join(groups)
     print(line)
 
