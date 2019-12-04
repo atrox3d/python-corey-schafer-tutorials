df48d0aa8fa7539cac972fa6b13b22774f2215b7
atrox3d <atrox3d@gmail.com>
2019-12-04 08:57:25 +0100

    add 21 re urls

---
+++ 21-re-urls.py
@@ -0,0 +1,20 @@
+#
+#   https://www.youtube.com/watch?v=K8L6KVGG-7o
+#
+#################################################################################
+import utils
+import re
+from collections import namedtuple
+import os
+
+
+urls = '''
+'''
+
+pattern = re.compile(r'')
+
+matches = pattern.finditer(urls)
+
+for match in matches:
+    print(match)
+
