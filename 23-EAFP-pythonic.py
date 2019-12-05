cf5f729f5e2bc47e3fe55b2e6b5366ccbbfeeadf
atrox3d <atrox3d@gmail.com>
2019-12-05 08:05:16 +0100

    add 23 EAFP examples

---
+++ 23-EAFP-pythonic.py
@@ -0,0 +1,17 @@
+#
+#   https://www.youtube.com/watch?v=x3v9zMX1s4s
+#
+#################################################################################
+from modules import utils
+from modules.duck import Duck
+from modules.person import Person
+
+#################################################################################
+#
+#
+#   ducktyping (pythonic)
+#   EAFP: easier ask forgiveness than permission
+#
+#
+#################################################################################
+utils.banner('ducktyping | pythonic | EAFP: easier ask forgiveness than permission')
