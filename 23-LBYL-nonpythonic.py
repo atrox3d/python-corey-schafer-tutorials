87e10f51a02d0b8899240014bc55d837a64b9ff8
atrox3d <atrox3d@gmail.com>
2019-12-05 08:04:42 +0100

    add 23 LBYL examples

---
+++ 23-LBYL-nonpythonic.py
@@ -0,0 +1,38 @@
+#
+#   https://www.youtube.com/watch?v=x3v9zMX1s4s
+#
+#################################################################################
+from modules import utils
+from modules.duck import Duck
+from modules.person import Person
+
+#################################################################################
+#
+#
+#   ducktyping (non pythonic)
+#   LBYL: look before you leap
+#
+#
+#################################################################################
+
+utils.banner('ducktyping | non pythonic | LBYL: look before you leap')
+
+
+def quackandfly(thing):
+    # LBYL
+    # non pythonic
+    # not duck-typed
+    if isinstance(thing, Duck):
+        print(f'ok, this is a {thing.__class__.__name__}')
+        thing.quack()
+        thing.fly()
+    else:
+        print(f'this is no duck, this is a {thing.__class__.__name__}')
+    print()
+
+
+duck = Duck()
+quackandfly(duck)
+
+person = Person()
+quackandfly(person)
