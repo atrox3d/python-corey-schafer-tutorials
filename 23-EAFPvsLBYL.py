74da1a999a61067c876c1b7219ef1001c8679027
atrox3d <atrox3d@gmail.com>
2019-12-05 10:07:57 +0100

    add 23-EAFPvsLBYL.py

---
+++ 23-EAFPvsLBYL.py
@@ -0,0 +1,76 @@
+#
+#   https://www.youtube.com/watch?v=x3v9zMX1s4s
+#
+#################################################################################
+from modules import utils
+
+person1 = dict(
+    name='jess',
+    age=23,
+    job='programmer'
+)
+
+person2 = dict(
+    name='jess',
+    age=23,
+)
+
+
+def printpersonLBYL(p):
+    if 'name' in p and 'age' in p and 'job' in p:
+        print("i'm {name}. i'm {age} and i am a {job}".format(**p))
+    else:
+        print('missing some keys')
+
+
+def printpersonEAFP(p):
+    try:
+        print("i'm {name}. i'm {age} and i am a {job}".format(**p))
+    except KeyError as ke:
+        print('missing {} key'.format(ke))
+
+
+########################################################################################################################
+#
+# print person LBYL
+#
+########################################################################################################################
+utils.banner('print person LBYL')
+printpersonLBYL(person1)
+printpersonLBYL(person2)
+########################################################################################################################
+#
+# print person EAFP
+#
+########################################################################################################################
+utils.banner('print person EAFP')
+printpersonEAFP(person1)
+printpersonEAFP(person2)
+
+
+########################################################################################################################
+def printlistLBYL(l):
+    if len(l) >= 6:
+        print(l[5])
+    else:
+        print('that index does not exist')
+
+
+def printlistEAFP(l):
+    try:
+        print(l[5])
+    except IndexError as ie:
+        print(ie)
+
+
+mylist = list(range(1, 6))
+
+idx = 0
+print(len(mylist))
+print(mylist)
+for i in mylist:
+    print(idx, i)
+    idx += 1
+
+printlistLBYL(mylist)
+printlistEAFP(mylist)
