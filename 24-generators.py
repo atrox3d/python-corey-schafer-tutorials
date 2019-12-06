3e207f9ba1519c1aea1442a74337c622a38d039b
atrox3d <atrox3d@gmail.com>
2019-12-06 14:31:35 +0100

    add 24-generators.py

---
+++ 24-generators.py
@@ -0,0 +1,32 @@
+#
+#   https://www.youtube.com/watch?v=x3v9zMX1s4s
+#
+#################################################################################
+from modules import utils
+
+
+#################################################################################
+#
+#
+#   generator functions
+#
+#
+#################################################################################
+def get_simplegenerator(n=10):
+    for x in range(1, n):
+        yield x
+
+
+gen = get_simplegenerator()
+
+utils.banner('generator functions')
+utils.printfvar('gen', gen)
+for g in gen:
+    utils.printfvar('g', g)
+
+gen = list(get_simplegenerator())
+
+utils.banner('generator functions as list')
+utils.printfvar('gen', gen)
+for g in gen:
+    utils.printfvar('g', g)
