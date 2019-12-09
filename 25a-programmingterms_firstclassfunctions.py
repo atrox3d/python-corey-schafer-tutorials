18622686c3effee72531cc030b164042a7cf80fd
atrox3d <atrox3d@gmail.com>
2019-12-09 08:35:36 +0100

    assigning functions to variables

--- 25a-programmingterms_firstclassfunctions.py
+++ 25a-programmingterms_firstclassfunctions.py
@@ -3,3 +3,27 @@
 #
 #################################################################################
 from modules import utils
+
+#################################################################################
+utils.banner('function assigning')
+
+
+def square(x):
+    return x * x
+
+
+utils.printfvar('square', square)
+#
+# assign return value to variable
+#
+f = square(5)
+utils.printfvar('f', f)
+
+print()
+#
+# assign function to variable
+#
+f = square
+utils.printfvar('f', f)
+utils.printfvar('f(5)', f(5))
+
