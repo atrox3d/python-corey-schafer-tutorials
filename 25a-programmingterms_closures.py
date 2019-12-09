b96416f09160537e83de5fc388c66b46db28bd91
atrox3d <atrox3d@gmail.com>
2019-12-09 13:56:53 +0100

    first form of closure

--- 25a-programmingterms_closures.py
+++ 25a-programmingterms_closures.py
@@ -5,4 +5,30 @@
 from modules import utils
 
 #################################################################################
-utils.banner('closures')
+utils.banner('closures: call inner function before returning its return value (None)')
+
+
+def outer_func():
+    #
+    #   free variable
+    #
+    message = "hi"
+
+    #
+    #   define an inner function
+    #
+    def inner_func():
+        print(f'inner_func: {message}')
+
+    #
+    #   returns None, after executing inner_func
+    #
+    return inner_func()
+
+
+#
+#   test this form of closure
+#
+x = outer_func()
+utils.printfvar('x', x)
+
