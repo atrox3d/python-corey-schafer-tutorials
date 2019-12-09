72f03dcea2bd0ab519271889d8a25b51633e8c8a
atrox3d <atrox3d@gmail.com>
2019-12-09 08:36:41 +0100

    high-order functions

--- 25a-programmingterms_firstclassfunctions.py
+++ 25a-programmingterms_firstclassfunctions.py
@@ -27,3 +27,43 @@ f = square
 utils.printfvar('f', f)
 utils.printfvar('f(5)', f(5))
 
+#################################################################################
+utils.banner('high-order functions')
+
+
+#
+#   custom defined map
+#
+def mymap(func, arglist):
+    """
+    executes func for every value in arglist
+    :returns a list containing the results
+    :param func: a function that accepts a value
+    :param arglist: a list of values to apply the func to
+    """
+    utils.printfvar(func.__name__, func)
+    utils.printfvar('arglist', arglist)
+    result = []
+    for i in arglist:
+        result.append(func(i))
+    utils.printfvar('result', result)
+    return result
+
+
+#
+#   apply square function to list
+#
+squares = mymap(square, [1, 2, 3, 4, 5])
+utils.printfvar('squares', squares)
+
+
+def cube(x):
+    return square(x) * x
+
+
+print()
+#
+#   apply cube function to list
+#
+cubes = mymap(cube, [1, 2, 3, 4, 5])
+utils.printfvar('cubes', cubes)
