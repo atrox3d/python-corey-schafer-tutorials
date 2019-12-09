ba0b4827281a983e4b85fd336cc5838ebc2b2fe2
atrox3d <atrox3d@gmail.com>
2019-12-09 14:32:17 +0100

    test decorators

--- 25a-programmingterms_closures_complex.py
+++ 25a-programmingterms_closures_complex.py
@@ -23,8 +23,33 @@ def logger(func):
     #   will run func(*args) after logging the call
     #
     def logfunc(*args):
+        #
+        #   log function call and arguments
+        #
         logging.info('running "{}" with arguments {}'.format(func.__name__, args))
+        #
+        #   execute func and print return value
+        #
         print(func(*args))
 
     return logfunc
 
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+#
+#   create two decorators
+#
+addlogger = logger(add)
+sublogger = logger(sub)
+#
+#   run them and check console/log file
+#
+addlogger(5, 5)
+sublogger(5, 5)
