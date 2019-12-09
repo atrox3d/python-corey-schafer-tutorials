c96e0447e39e51a153270b8d2a1ea1b6a9e0e214
atrox3d <atrox3d@gmail.com>
2019-12-09 15:09:16 +0100

    create simple decorator

--- 25d-decorators.py
+++ 25d-decorators.py
@@ -2,3 +2,22 @@
 #   https://www.youtube.com/watch?v=FsAPt_9Bf3U
 #
 #################################################################################
+from modules import utils
+
+
+utils.banner('decorators')
+
+
+def decorator_function(original_function):
+    def wrapper_function():
+        return original_function()
+
+    return wrapper_function
+
+
+def display():
+    print('display function ran')
+
+
+decorated_display = decorator_function(display)
+decorated_display()
