8dc325f317d0402aa4ba3580bea879de0dbeb4ed
atrox3d <atrox3d@gmail.com>
2019-12-10 09:58:38 +0100

    add prefix to decorator

--- 26-decoratorwithparameters.py
+++ 26-decoratorwithparameters.py
@@ -5,17 +5,19 @@
 from modules import utils
 
 
-def decorator_function(original_function):
-    def wrapper_function(*args, **kwargs):
-        print(f'wrapper executed before {original_function.__name__} ({args}, {kwargs})')
-        result = original_function(*args, **kwargs)
-        print(f'wrapper executed after {original_function.__name__} ({args}, {kwargs})')
-        return result
+def prefix_decorator(prefix):
+    def decorator_function(original_function):
+        def wrapper_function(*args, **kwargs):
+            print(f'{prefix} wrapper executed before {original_function.__name__} ({args}, {kwargs})')
+            result = original_function(*args, **kwargs)
+            print(f'{prefix} wrapper executed after {original_function.__name__} ({args}, {kwargs})')
+            return result
 
-    return wrapper_function
+        return wrapper_function
+    return decorator_function
 
 
-@decorator_function
+@prefix_decorator('LOG: ')
 def displayinfo(name, age):
     print(f'displayinfo ran with arguments ({name}, {age})')
 
