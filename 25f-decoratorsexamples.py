b6baecaf1db56447afd18e433b98634727a599df
atrox3d <atrox3d@gmail.com>
2019-12-10 08:40:26 +0100

    use logger decorator

--- 25f-decoratorsexamples.py
+++ 25f-decoratorsexamples.py
@@ -5,7 +5,7 @@
 from modules import utils
 
 #################################################################################
-utils.banner('decorator practical examples')
+utils.banner('decorator practical examples: logger')
 
 
 def mylogger(func):
@@ -15,14 +15,28 @@ def mylogger(func):
     #   create logfile path and configure logger
     #
     filename = os.path.join(os.getcwd(), 'data', f'{func.__name__}.log')
+    print(f'logfile: {filename}')
     logging.basicConfig(filename=filename, level=logging.INFO)
 
     def wrapper(*args, **kwargs):
         logging.info(
-            'ran with args: {} and kwargs: {}'.format(args, kwargs)
+            # 'ran with args: {} and kwargs: {}'.format(args, kwargs)
+            f'ran with args: {args} and kwargs: {kwargs}'
         )
         return func(*args, **kwargs)
 
     return wrapper
 
 
+# @mylogger
+# def display():
+#     print('display function ran')
+
+
+@mylogger
+def displayinfo(name, age):
+    print(f'displayinfo ran with arguments ({name}, {age})')
+
+
+# display()
+displayinfo('john', 25)
