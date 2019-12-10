b97f05b66c2bc48de78474a3b854af64a8fdd74f
atrox3d <atrox3d@gmail.com>
2019-12-10 08:15:33 +0100

    implement class decorator

--- 25e-classdecorators.py
+++ 25e-classdecorators.py
@@ -8,14 +8,22 @@ from modules import utils
 utils.banner('decorators normal syntax')
 
 
-# TODO: implement class decorator
+class DecoratorClass(object):
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print(f'call method executed this before {self.original_function.__name__}')
+        return self.original_function(*args, **kwargs)
+
 
 def display():
     print('display function ran')
 
 
-decorated_display = decorator_function(display)
+decorated_display = DecoratorClass(display)
 decorated_display()
+
 
 #################################################################################
 utils.banner('decorators annotated syntax')
@@ -24,12 +32,12 @@ utils.banner('decorators annotated syntax')
 #
 #   this is the same as:
 #           decorated_display = decorator_function(display)
-@decorator_function
+@DecoratorClass
 def display():
     print('display function ran')
 
 
-@decorator_function
+@DecoratorClass
 def displayinfo(name, age):
     print(f'displayinfo ran with arguments ({name}, {age})')
 
