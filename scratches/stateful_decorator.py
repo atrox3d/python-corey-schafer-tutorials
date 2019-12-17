6ab5e78b478994e44d02879e53c3ca8ed5c6962b
atrox3d <atrox3d@gmail.com>
2019-12-17 09:26:25 +0100

    experiment with decorator attributes

--- scratches/stateful_decorator.py
+++ scratches/stateful_decorator.py
@@ -7,8 +7,12 @@ def stateful_decorator(func):
 
     returns statefulwrapper
     """
-    stateful_decorator.count = stateful_decorator.count + 1 if hasattr(stateful_decorator, 'count') else 0
-    print(f'decorator usage: {stateful_decorator.count}')
+
+    #
+    #   setup counter hattribute
+    #
+    stateful_decorator.counter = stateful_decorator.counter + 1 if hasattr(stateful_decorator, 'counter') else 0
+    print(f'stateful_decorator | decorator usage: {stateful_decorator.counter}')
 
     @functools.wraps(func)
     def stateful_wrapper(*args, **kwargs):
@@ -20,8 +24,9 @@ def stateful_decorator(func):
         # stateful_wrapper.wrapperfunc = stateful_wrapper.__name__
         stateful_wrapper.wrapperfunc = "stateful_wrapper"
         stateful_wrapper.wrappedfunc = func.__name__
-        print(f'wrapper function: {stateful_wrapper.wrapperfunc}')
-        print(f'wrapped function: {stateful_wrapper.wrappedfunc}')
+        print(f'stateful_wrapper   | wrapper function : {stateful_wrapper.wrapperfunc}')
+        print(f'stateful_wrapper   | wrapped function : {stateful_wrapper.wrappedfunc}')
+        print(f'stateful_wrapper   | decorator usage  : {stateful_decorator.counter}')
         value = func(*args, **kwargs)
         return value
 
@@ -35,6 +40,11 @@ def hello(name='bob'):
 
 @stateful_decorator
 def hello(name='bob'):
+    print(f'hello {name}')
+
+
+@stateful_decorator
+def hellox(name='bob'):
     print(f'hello {name}')
 
 
