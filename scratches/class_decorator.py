846ef6430c76486830776506e7d16625f9e3466f
atrox3d <atrox3d@gmail.com>
2019-12-17 10:28:52 +0100

    implement class as a decorator

--- scratches/class_decorator.py
+++ scratches/class_decorator.py
@@ -8,3 +8,35 @@ utils.banner(
 )
 
 
+class CountCalls:
+    def __init__(self, func):
+        """
+        The .__init__() method must store a reference to the function and can do any other necessary initialization.
+
+        Note that you need to use the functools.update_wrapper() function instead of @functools.wraps.
+
+        :param func:
+        """
+        functools.update_wrapper(self, func)
+        self.func = func
+        self.num_calls = 0
+
+    def __call__(self, *args, **kwargs):
+        """
+        The .__call__() method will be called instead of the decorated function. It does essentially the same thing as the wrapper() function in our earlier examples.
+
+        :param args:
+        :param kwargs:
+        :return: func(*args, **kwargs)
+        """
+        self.num_calls += 1
+        print(f'call {self.num_calls} of {self.func.__name__!r}')
+        return self.func(*args, **kwargs)
+
+
+@CountCalls
+def say_whee():
+    print('whee')
+
+
+say_whee()
