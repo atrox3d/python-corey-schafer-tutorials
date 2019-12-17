fa1b2203dd4cdb5705790db9709b69ca70d80dbe
atrox3d <atrox3d@gmail.com>
2019-12-17 09:54:23 +0100

    add class_decorator.py

---
+++ scratches/class_decorator.py
@@ -0,0 +1,10 @@
+import functools
+from modules import utils
+
+########################################################################################################################
+utils.banner(
+    'https://realpython.com/primer-on-python-decorators/#classes-as-decorators',
+    'class as decorator'
+)
+
+
