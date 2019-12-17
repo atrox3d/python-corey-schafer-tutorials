0893da86fe88b062dddbff4498bae744cab3879a
atrox3d <atrox3d@gmail.com>
2019-12-17 14:49:43 +0100

    empty class with dynamic properties

--- 02-OOP/01-classes and instances.py
+++ 02-OOP/01-classes and instances.py
@@ -1,2 +1,15 @@
 from modules import utils
 
+
+class Employee:
+    pass
+
+
+emp1 = Employee()
+emp2 = Employee()
+
+utils.printfvar(emp1, 'emp1')
+utils.printfvar(emp2, 'emp2')
+
+emp1.name = 'bob'
+print(emp1.name)
