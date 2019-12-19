a4672f4d5fb5272653e37aa932045717852f0652
atrox3d <atrox3d@gmail.com>
2019-12-19 14:51:08 +0100

    setup project paths

--- 03-python practical examples/01-beautiful soup.py
+++ 03-python practical examples/01-beautiful soup.py
@@ -1,7 +1,11 @@
 from modules import utils
+from bs4 import BeautifulSoup
+import requests
 
 utils.banner(
     'https://www.youtube.com/watch?v=ng2o98k983k',
-    'web scraping jwith beautiful soup'
+    'web scraping with beautiful soup',
+    f'PROJECT_PATH    : {utils.PROJECT_PATH}',
+    f'PROJECTDATA_PATH: {utils.PROJECTDATA_PATH}',
 )
 
