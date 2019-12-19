43b1ba0417105ff7cda91e641d6b1e26bd53246b
atrox3d <atrox3d@gmail.com>
2019-12-19 09:29:33 +0100

    add setter and deleter

--- 02-OOP/06-get set delete.py
+++ 02-OOP/06-get set delete.py
@@ -13,10 +13,46 @@ class Employee:
 
         Employee.count += 1
 
+    #
+    # getter
+    #
     @property
     def fullname(self):
+        """
+        combines first<space>last
+
+        :return: fullname
+        """
         return f'{self.first} {self.last}'
 
+    #
+    # setter
+    #
+    @fullname.setter
+    def fullname(self, name):
+        """
+        split full name by <space>
+
+        assigns the result to first and last
+
+        :param name:
+        """
+        self.first, self.last = name.split(' ')
+
+    #
+    # deleter
+    #
+    @fullname.deleter
+    def fullname(self):
+        """
+        deletes both first and last
+        """
+        self.first = None
+        self.last = None
+
+    #
+    # only getter
+    #
     @property
     def email(self):
         """
@@ -38,8 +74,20 @@ utils.banner(
 
 emp1 = Employee('bob', 'red', 5000)
 
+utils.banner('change name from bob to Jim')
 emp1.first = 'Jim'
+print(emp1.first)
+print(emp1.email)
+print(emp1.fullname)
 
+utils.banner('change fullname to rob robbins')
+emp1.fullname = 'rob robbins'
+print(emp1.first)
+print(emp1.email)
+print(emp1.fullname)
+
+utils.banner('del fullname')
+del emp1.fullname
 print(emp1.first)
 print(emp1.email)
 print(emp1.fullname)
