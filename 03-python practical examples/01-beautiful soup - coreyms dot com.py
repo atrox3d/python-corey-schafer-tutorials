11be83c7632c8c64dd5731d25b4c82d0189e41da
atrox3d <atrox3d@gmail.com>
2019-12-20 09:17:04 +0100

    add 01-beautiful soup - coreyms dot com.py

---
+++ 03-python practical examples/01-beautiful soup - coreyms dot com.py
@@ -0,0 +1,12 @@
+from modules import utils
+import os.path
+from bs4 import BeautifulSoup
+import requests
+
+########################################################################################################################
+utils.banner(
+    'https://www.youtube.com/watch?v=ng2o98k983k',
+    'web scraping with beautiful soup',
+    'http://coreyms.com'
+)
+
