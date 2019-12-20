54f6cf5b5205a19a642f237223c6aa4d44427350
atrox3d <atrox3d@gmail.com>
2019-12-20 07:49:23 +0100

    soup match and find

--- 03-python practical examples/01-beautiful soup.py
+++ 03-python practical examples/01-beautiful soup.py
@@ -3,6 +3,7 @@ import os.path
 from bs4 import BeautifulSoup
 import requests
 
+########################################################################################################################
 utils.banner(
     'https://www.youtube.com/watch?v=ng2o98k983k',
     'web scraping with beautiful soup',
@@ -20,6 +21,37 @@ with open(html_path) as html_file:              #
 with open(html_path) as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
+########################################################################################################################
 utils.banner('HTML SOUP DUMP (prettified):')
 print(soup.prettify())
 
+########################################################################################################################
+utils.banner('matching tags')
+match_title = soup.title
+print(f'.title:')
+utils.hashline(char='-')
+print(f'{match_title}')
+utils.hashline(char='-')
+print(f'.title.text:')
+utils.hashline(char='-')
+print(f'{match_title.text}')
+print()
+match_div = soup.div
+utils.hashline(char='-')
+print(f'.div:')
+utils.hashline(char='-')
+print(f'{match_div}')
+
+########################################################################################################################
+utils.banner('find tags')
+
+find_div = soup.find('div')
+print(f'find div:')
+utils.hashline(char='-')
+print(f'{find_div}')
+
+find_divfooter = soup.find('div', class_='footer')
+utils.hashline(char='-')
+print(f'find div/footer:')
+utils.hashline(char='-')
+print(f'{find_divfooter}')
