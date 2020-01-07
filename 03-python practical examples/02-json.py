32177e8f6e589192430735067c5fd8efbc424abf
atrox3d <atrox3d@gmail.com>
2020-01-07 14:55:00 +0100

    simple JSON example

--- 03-python practical examples/02-json.py
+++ 03-python practical examples/02-json.py
@@ -18,7 +18,28 @@ people_string = '''
             "phone": "560-555-5153",
             "emails": null,
             "has_license": true
-        },
+        }
     ]
 }
 '''
+
+json_data = json.loads(people_string)
+print(json_data)
+print()
+
+for person in json_data['people']:
+    print(f'{person}')
+print()
+
+for person in json_data['people']:
+    print(f'{person["name"]!r}')
+print()
+
+for person in json_data['people']:
+    del person['phone']
+
+new_json = json.dumps(json_data, indent=2, sort_keys=True)
+print(new_json)
+print()
+
+
