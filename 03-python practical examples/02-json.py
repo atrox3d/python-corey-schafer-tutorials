10cbac823f98168251e4b8d1342c85d0b9fc60b6
atrox3d <atrox3d@gmail.com>
2020-01-07 14:35:20 +0100

    add 02-json.py

---
+++ 03-python practical examples/02-json.py
@@ -0,0 +1,24 @@
+''' JavaScript Object Notation '''
+import json
+
+people_string = '''
+{
+    "people": [
+        {
+            "name": "john smith",
+            "phone": "615-555-7164",
+            "emails": [
+                "johnsmith@bogusemail.com",
+                "john.smith@work-place.com"
+            ],
+            "has_license": false
+        },
+        {
+            "name": "jane doe",
+            "phone": "560-555-5153",
+            "emails": null,
+            "has_license": true
+        },
+    ]
+}
+'''
