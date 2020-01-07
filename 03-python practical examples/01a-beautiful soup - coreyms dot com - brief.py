10b034ae72c4b6678a26a10bc074bf57183e24fe
atrox3d <atrox3d@gmail.com>
2020-01-07 14:01:22 +0100

    final cleanup

--- 03-python practical examples/01a-beautiful soup - coreyms dot com - brief.py
+++ 03-python practical examples/01a-beautiful soup - coreyms dot com - brief.py
@@ -11,16 +11,16 @@ utils.banner(
     'http://coreyms.com'
 )
 
-request = requests.get('http://coreyms.com')                        # execute request
-print(f'request to http://coreyms.com: {request}')                  # print request http status
+request = requests.get('http://coreyms.com')                            # execute request
+print(f'request to http://coreyms.com: {request}')                      # print request http status
 
-source = request.text                                               # extract request text (html)
+source = request.text                                                   # extract request text (html)
 ########################################################################################################################
 utils.banner('parse source with BeautifulSoup (prettify)')
 
-soup = BeautifulSoup(source, 'lxml')                                # parse html with BeautifulSoup via lxml
+soup = BeautifulSoup(source, 'lxml')                                    # parse html with BeautifulSoup via lxml
 ########################################################################################################################
-for article in soup.find_all('article'):                                      # find tag article inside soup obj
+for article in soup.find_all('article'):                                # find tag article inside soup obj
     headline = article.h2.a.text                                        # navigate to link text
     headline2 = article.a.text                                          # same, different syntax
     print(f'{headline2!r}')
@@ -31,13 +31,13 @@ for article in soup.find_all('article'):                                      # 
     video = article.find('iframe')                                      # get iframe inside article
 
     if video is not None:
-        video_src = video['src']                                            # display video src
+        video_src = video['src']                                        # display video src
         video_url = video_src.split('?')[0]
-        # print('video url w/o query parameters: ', video_url)
 
         videourl_parts = video_url.split('/')
         video_id = videourl_parts[-1]
 
         yt_link = f'https://youtube.com/watch?v={video_id}'
         print(yt_link)
+
     print()
