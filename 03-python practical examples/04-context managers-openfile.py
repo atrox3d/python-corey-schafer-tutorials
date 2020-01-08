698cc13951927f11c89a24d2162b0bdfcbbad611
atrox3d <atrox3d@gmail.com>
2020-01-08 13:57:14 +0100

    add method decorator for trace

--- 03-python practical examples/04-context managers-openfile.py
+++ 03-python practical examples/04-context managers-openfile.py
@@ -1,16 +1,28 @@
 from modules import utils
 
 
-class OpenFile:
+def logmethod(fn):
+    def wrapped(*args, **kwargs):
+        print(f'entering {fn.__name__}')
+        value = fn(*args, **kwargs)
+        print(f'exiting {fn.__name__}')
+        return value
 
+    return wrapped
+
+
+class OpenFile:
+    @logmethod
     def __init__(self, filename, mode='r'):
         self.filename = filename
         self.mode = mode
 
+    @logmethod
     def __enter__(self):
         self.file = open(self.filename, self.mode)
         return self.file
 
+    @logmethod
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.file.close()
 
