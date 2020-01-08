8ff6a199d710bfbd294620da80b1765140227042
atrox3d <atrox3d@gmail.com>
2020-01-08 12:13:22 +0100

    add class OpenFile

--- 03-python practical examples/04-context managers-openfile.py
+++ 03-python practical examples/04-context managers-openfile.py
@@ -1,1 +1,16 @@
 from modules import utils
+
+
+class OpenFile:
+
+    def __init__(self, filename, mode='r'):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
