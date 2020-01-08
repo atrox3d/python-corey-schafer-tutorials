7fc7a65994f80fe606f945bc4d3a9d60d3e1d5a4
atrox3d <atrox3d@gmail.com>
2020-01-08 14:18:24 +0100

    add try

--- 03-python practical examples/04-context managers-openfile.py
+++ 03-python practical examples/04-context managers-openfile.py
@@ -36,12 +36,14 @@ print(f.closed)
 
 @contextmanager
 def openfile(file, mode='r'):
-    print(f'opening file: {file} with mode: {mode}')
-    f = open(file, mode)
-    print(f'yielding file: {file}')
-    yield f
-    print(f'closing file: {file}')
-    f.close()
+    try:
+        print(f'opening file: {file} with mode: {mode}')
+        f = open(file, mode)
+        print(f'yielding file: {file}')
+        yield f
+    finally:
+        print(f'closing file: {file}')
+        f.close()
 
 
 with openfile(utils.getdatafilepath('sample.txt'), 'r+') as f:
