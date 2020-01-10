cd8a58dc659e660ee0a7bb1ad425b1c69907f742
atrox3d <atrox3d@gmail.com>
2020-01-10 14:03:40 +0100

    default level is warning, debug does not display

--- 03-python practical examples/06-log-sample.py
+++ 03-python practical examples/06-log-sample.py
@@ -2,6 +2,20 @@
 #   https://youtu.be/-ARI4Cz-awo
 #
 #########################################################################################################
+import logging
+
+
+# DEBUG: Detailed information, typically of interest only when diagnosing problems.
+
+# INFO: Confirmation that things are working as expected.
+
+# WARNING: An indication that something unexpected happened, or indicative of some problem in the near future
+# (e.g. ‘disk space low’). The software is still working as expected.
+
+# ERROR: Due to a more serious problem, the software has not been able to perform some function.
+
+# CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+
 
 def add(x, y):
     """Add Function"""
@@ -27,13 +41,13 @@ num_1 = 20
 num_2 = 10
 
 add_result = add(num_1, num_2)
-print('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+logging.warning('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 
 sub_result = subtract(num_1, num_2)
-print('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 
 mul_result = multiply(num_1, num_2)
-print('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 
 div_result = divide(num_1, num_2)
-print('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
