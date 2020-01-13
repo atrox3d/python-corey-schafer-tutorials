11d5d164fef09374b6d80149892ba143f4cefcc6
atrox3d <atrox3d@gmail.com>
2020-01-13 10:49:06 +0100

    rename to 29__name__.py for import

--- 01-python-basics/29__name__.py
+++ 01-python-basics/29__name__.py
@@ -3,8 +3,10 @@
 #
 #################################################################################
 from modules import utils
+import os
 
 utils.banner(
     'https://youtu.be/sugvnHA7ElY',
     "if __name__ == '__main__'"
 )
+print(f'"{os.path.basename(__file__)}"\'s name: "{__name__}"')
