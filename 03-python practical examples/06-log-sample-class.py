e5a8c2041b8ca25e95611096ef1e383fe5435473
atrox3d <atrox3d@gmail.com>
2020-01-13 07:46:57 +0100

    make code like video example

--- 03-python practical examples/06-log-sample-class.py
+++ 03-python practical examples/06-log-sample-class.py
@@ -23,7 +23,8 @@ class Employee:
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
-        self.pay = pay
+
+        print('Created Employee: {} - {}'.format(self.fullname, self.email))
 
     @property
     def email(self):
@@ -33,7 +34,5 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
 
 
