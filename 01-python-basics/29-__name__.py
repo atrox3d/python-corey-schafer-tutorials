7965d20fef0f2950eb35c2943a765434088ff4d2
atrox3d <atrox3d@gmail.com>
2020-01-13 10:43:31 +0100

    add 29-__name__.py

---
+++ 01-python-basics/29-__name__.py
@@ -0,0 +1,10 @@
+#
+#   https://youtu.be/sugvnHA7ElY
+#
+#################################################################################
+from modules import utils
+
+utils.banner(
+    'https://youtu.be/sugvnHA7ElY',
+    "if __name__ == '__main__'"
+)
