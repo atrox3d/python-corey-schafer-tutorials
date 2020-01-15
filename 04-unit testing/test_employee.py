bc1db3b9ace4cd9dfb793608e0190b9097a5e205
atrox3d <atrox3d@gmail.com>
2020-01-15 12:13:13 +0100

    add test_employee.py

---
+++ 04-unit testing/test_employee.py
@@ -0,0 +1,14 @@
+import unittest
+import employee
+
+
+class TestEmployee(unittest.TestCase):
+
+    def test_email(self):
+        self.fail()
+
+    def test_fullname(self):
+        self.fail()
+
+    def test_apply_raise(self):
+        self.fail()
