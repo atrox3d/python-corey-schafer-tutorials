46399823f119bc057def1da98679b9e786cc1a57
atrox3d <atrox3d@gmail.com>
2020-01-15 08:55:33 +0100

    add first test

--- 04-unit testing/test_calc.py
+++ 04-unit testing/test_calc.py
@@ -31,4 +31,5 @@ class TestCalc(unittest.TestCase):
     ----------------------------+--------------------------------------------
     """
     def test_add(self):
-        pass
+        result = calc.add(10, 5)
+        self.assertEqual(result, 15)
