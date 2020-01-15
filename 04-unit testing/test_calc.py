3b173d15515ffc50bd243764d2689649e68cef56
atrox3d <atrox3d@gmail.com>
2020-01-15 09:03:08 +0100

    reformat comments

--- 04-unit testing/test_calc.py
+++ 04-unit testing/test_calc.py
@@ -5,31 +5,35 @@
 this is the test unit for module calc.py
 it is conventionally named test_*
 """
-import unittest     # std library
-import calc         # in pycharm: right click on folder/mark as/Sources Root
+import unittest  # std library
+import calc  # in pycharm: right click on folder/mark as/Sources Root
+
+"""
+https://docs.python.org/3/library/unittest.html#unittest.TestCase.debug
+----------------------------+--------------------------------------------
+Method					    |	Checks that
+----------------------------+--------------------------------------------
+assertEqual(a, b)		    |	a == b
+assertNotEqual(a, b)	    |	a != b
+assertTrue(x)			    |	bool(x) is True
+assertFalse(x)			    |	bool(x) is False
+assertIs(a, b)			    |	a is b
+assertIsNot(a, b)		    |	a is not b
+assertIsNone(x)			    |	x is None
+assertIsNotNone(x)		    |	x is not None
+assertIn(a, b)			    |	a in b
+assertNotIn(a, b)		    |	a not in b
+assertIsInstance(a, b)	    |	isinstance(a, b)
+assertNotIsInstance(a, b)   |	not isinstance(a, b)
+----------------------------+--------------------------------------------
+"""
 
 
 class TestCalc(unittest.TestCase):
     """
-    https://docs.python.org/3/library/unittest.html#unittest.TestCase.debug
-
-    ----------------------------+--------------------------------------------
-    Method					    |	Checks that
-    ----------------------------+--------------------------------------------
-    assertEqual(a, b)		    |	a == b
-    assertNotEqual(a, b)	    |	a != b
-    assertTrue(x)			    |	bool(x) is True
-    assertFalse(x)			    |	bool(x) is False
-    assertIs(a, b)			    |	a is b
-    assertIsNot(a, b)		    |	a is not b
-    assertIsNone(x)			    |	x is None
-    assertIsNotNone(x)		    |	x is not None
-    assertIn(a, b)			    |	a in b
-    assertNotIn(a, b)		    |	a not in b
-    assertIsInstance(a, b)	    |	isinstance(a, b)
-    assertNotIsInstance(a, b)   |	not isinstance(a, b)
-    ----------------------------+--------------------------------------------
+    python -m unittest test_calc.py
     """
+
     def test_add(self):
         result = calc.add(10, 5)
         self.assertEqual(result, 15)
