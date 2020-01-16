443bc93c228bbe0e7179f3821a9a15fb5c9223b3
atrox3d <atrox3d@gmail.com>
2020-01-16 14:06:24 +0100

    add mock.patch decorator

--- mocking/example2/test_rm.py
+++ mocking/example2/test_rm.py
@@ -3,19 +3,21 @@ from mymodule import rm
 import os.path
 import tempfile
 import unittest
+from unittest import mock
 
 
 class RmTestCase(unittest.TestCase):
-
     tmppfilepath = os.path.join(tempfile.gettempdir(), 'tmp-testfile')
 
     def setUp(self) -> None:
         with open(self.tmppfilepath, 'wb') as f:
             f.write(b"delete me")
 
-    def test_rm(self):
-        rm(self.tmppfilepath)
-        self.assertFalse(os.path.isfile(self.tmppfilepath), 'failed to remove the file')
+    @mock.patch('mymodule.os')
+    def test_rm_decorator(self, mock_os):
+        rm("any path")
+        # self.assertFalse(os.path.isfile(self.tmppfilepath), 'failed to remove the file')
+        mock_os.remove.assert_called_with("any path")
 
 
 if __name__ == '__main__':
