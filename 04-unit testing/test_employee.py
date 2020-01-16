360bd84a89e4cab01246653779a5f230ecfcd266
atrox3d <atrox3d@gmail.com>
2020-01-16 08:30:13 +0100

    add mock patch

--- 04-unit testing/test_employee.py
+++ 04-unit testing/test_employee.py
@@ -1,5 +1,7 @@
 import unittest
-from employee import Employee
+from unittest.mock import patch
+# from employee_test import Employee
+from modules import employee_test
 
 
 class TestEmployee(unittest.TestCase):
@@ -7,8 +9,8 @@ class TestEmployee(unittest.TestCase):
     def setUp(self):
         print('setUp')
 
-        self.emp_1 = Employee('Corey', 'Schafer', 50000)
-        self.emp_2 = Employee('Sue', 'Smith', 60000)
+        self.emp_1 = employee_test.Employee('Corey', 'Schafer', 50000)
+        self.emp_2 = employee_test.Employee('Sue', 'Smith', 60000)
 
     def tearDown(self):
         print('tearDown\n')
@@ -53,6 +55,15 @@ class TestEmployee(unittest.TestCase):
 
         self.assertEqual(self.emp_1.pay, 52500)
         self.assertEqual(self.emp_2.pay, 63000)
+
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = 'Success'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/Schafer/May')
+            self.assertEqual(schedule, 'Success')
 
 
 if __name__ == '__main__':
