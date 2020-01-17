6ce1f05e41a26bbedb79137f39492a162bcaccc6
atrox3d <atrox3d@gmail.com>
2020-01-17 08:58:35 +0100

    add validation test

--- mocking/example2/test_rm.py
+++ mocking/example2/test_rm.py
@@ -16,9 +16,27 @@ class RmTestCase(unittest.TestCase):
         with open(self.tmppfilepath, 'wb') as f:
             f.write(b"delete me")
 
+    @mock.patch('mymodule.os.path')
     @mock.patch('mymodule.os')
-    def test_rm_decorator(self, mock_os):
+    def test_rm_decorator(self, mock_os, mock_path):
+        # print(mock_os, mock_path)
+
+        # setup the mock
+        mock_path.isfile.return_value = False
+
         rm("any path")
+
+        # test that the remove call was NOT called
+        self.assertFalse(
+            mock_os.remove.called,
+            "failed to not remove the file if not present"
+        )
+
+        # make the file exist
+        mock_path.isfile.return_value = True
+
+        rm("any path")
+
         # self.assertFalse(os.path.isfile(self.tmppfilepath), 'failed to remove the file')
         mock_os.remove.assert_called_with("any path")
 
