4df634059caee7a5a51540fc95057e672471ff6a
atrox3d <atrox3d@gmail.com>
2020-01-17 18:12:35 +0100

    format output

--- mocking/example2/mymodule.py
+++ mocking/example2/mymodule.py
@@ -8,11 +8,11 @@ class RemovalService(object):
     """A service for removing objects from the filesystem."""
 
     def rm(self, filename):
-        print(f'{__class__.__name__}.{self.rm.__name__}|check file {filename!r}')
+        print(f'{__class__.__name__}.{self.rm.__name__} | check file {filename!r}')
         if os.path.isfile(filename):
-            print(f'{__class__.__name__}.{self.rm.__name__}|{filename!r} exists')
-            print(f'{__class__.__name__}.{self.rm.__name__}|call os.remove({filename!r})')
+            print(f'{__class__.__name__}.{self.rm.__name__} | {filename!r} exists')
+            print(f'{__class__.__name__}.{self.rm.__name__} | call os.remove({filename!r})')
             os.remove(filename)
         else:
-            print(f'{__class__.__name__}.{self.rm.__name__}|{filename} does not exists')
+            print(f'{__class__.__name__}.{self.rm.__name__} | {filename} does not exists')
 
