3bbd99aad965e3b5037fff7c6bef1db0763ad96f
atrox3d <atrox3d@gmail.com>
2020-01-17 10:06:16 +0100

    add important explanation about stacked decorators order

--- mocking/example2/test_rm.py
+++ mocking/example2/test_rm.py
@@ -16,6 +16,27 @@ class RmTestCase(unittest.TestCase):
         with open(self.tmppfilepath, 'wb') as f:
             f.write(b"delete me")
 
+    """
+    MOCK PATCH PITFALL: DECORATOR ORDER
+    When using multiple decorators on your test methods, order is important, and it’s kind of confusing. 
+    Basically, when mapping decorators to method parameters, work backwards. Consider this example:
+    
+        @mock.patch('mymodule.sys')
+        @mock.patch('mymodule.os')
+        @mock.patch('mymodule.os.path')
+        def test_something(self, mock_os_path, mock_os, mock_sys):
+            pass
+    Notice how our parameters are matched to the reverse order of the decorators? 
+    That’s partly because of the way that Python works. With multiple method decorators, 
+    here’s the order of execution in pseudocode:
+    
+    patch_sys(patch_os(patch_os_path(test_something)))
+    Since the patch to sys is the outermost patch, it will be executed last, 
+    making it the last parameter in the actual test method arguments. 
+    Take note of this well and use a debugger when running your tests to make sure that the right parameters are being 
+    injected in the right order.    
+    """
+
     @mock.patch('mymodule.os.path')
     @mock.patch('mymodule.os')
     def test_rm_decorator(self, mock_os, mock_path):
