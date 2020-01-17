b7d38ae7c46e571c5a2b3436b7997357fe4c77b7
atrox3d <atrox3d@gmail.com>
2020-01-17 11:39:30 +0100

    refactor to class (service)

--- mocking/example2/mymodule.py
+++ mocking/example2/mymodule.py
@@ -4,6 +4,9 @@ https://www.toptal.com/python/an-introduction-to-mocking-in-python
 import os
 
 
-def rm(filename):
-    if os.path.isfile(filename):
-        os.remove(filename)
+class RemovalService(object):
+    """A service for removing objects from the filesystem."""
+
+    def rm(self, filename):
+        if os.path.isfile(filename):
+            os.remove(filename)
