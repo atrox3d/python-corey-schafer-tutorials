740fd137e938e468ab4e88f32d041d4e8479dbfa
atrox3d <atrox3d@gmail.com>
2020-01-17 09:27:00 +0100

    context manager version

--- mocking/example2/test_rm.py
+++ mocking/example2/test_rm.py
@@ -42,9 +42,27 @@ class RmTestCase(unittest.TestCase):
 
     def test_rm_context(self):
         with mock.patch('mymodule.os') as mock_os:
-            rm("any path")
-            # self.assertFalse(os.path.isfile(self.tmppfilepath), 'failed to remove the file')
-            mock_os.remove.assert_called_with("any path")
+            with mock.patch('mymodule.os.path') as mock_path:
+                # print(mock_os, mock_path)
+
+                # setup the mock
+                mock_path.isfile.return_value = False
+
+                rm("any path")
+
+                # test that the remove call was NOT called
+                self.assertFalse(
+                    mock_os.remove.called,
+                    "failed to not remove the file if not present"
+                )
+
+                # make the file exist
+                mock_path.isfile.return_value = True
+
+                rm("any path")
+
+                # self.assertFalse(os.path.isfile(self.tmppfilepath), 'failed to remove the file')
+                mock_os.remove.assert_called_with("any path")
 
 
 if __name__ == '__main__':
