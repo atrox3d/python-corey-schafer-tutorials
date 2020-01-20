d97be2e7ad5a95919e36ec8dbccc8a441c6b0de9
atrox3d <atrox3d@gmail.com>
2020-01-20 10:50:58 +0100

    setup class logger

--- mocking/example2/mymodule.py
+++ mocking/example2/mymodule.py
@@ -2,6 +2,32 @@
 https://www.toptal.com/python/an-introduction-to-mocking-in-python
 """
 import os
+import logging
+
+"""
+########################################################################################################################
+    - GET LOCAL (NON-ROOT) LOGGER INSTANCE
+    - SET LEVEL TO INFO (DEFAULT IS WARNING)
+########################################################################################################################
+"""
+logger = logging.getLogger(__name__)    # get local logger
+logger.setLevel(logging.DEBUG)          # set logger level >= INFO
+"""
+########################################################################################################################
+    - GET SAME FORMATTER INSTANCE FOR ALL HANDLERS
+########################################################################################################################
+"""
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:RemovalService.%(funcname)s:%(message)s')  # get formatter
+"""
+########################################################################################################################
+    - GET CLI HANDLER INSTANCE
+    - SET FORMATTER FOR CLI HANDLER INSTANCE
+    - ADD HANDLER TO LOCAL LOGGER
+########################################################################################################################
+"""
+cli_handler = logging.StreamHandler()   # get CLI handler (default=stderr)
+cli_handler.setFormatter(formatter)     # set formatter for CLI handler
+logger.addHandler(cli_handler)          # add CLI handler to logger
 
 
 class RemovalService(object):
@@ -15,4 +41,3 @@ class RemovalService(object):
             os.remove(filename)
         else:
             print(f'{__class__.__name__}.{self.rm.__name__}|{filename} does not exists')
-
