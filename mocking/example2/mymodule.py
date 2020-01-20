79dadd7bfe9f2fd53c0d94535e292d28a8f36fdf
atrox3d <atrox3d@gmail.com>
2020-01-20 11:33:55 +0100

    cleanup

--- mocking/example2/mymodule.py
+++ mocking/example2/mymodule.py
@@ -10,14 +10,15 @@ import logging
     - SET LEVEL TO INFO (DEFAULT IS WARNING)
 ########################################################################################################################
 """
-logger = logging.getLogger(__name__)    # get local logger
-logger.setLevel(logging.DEBUG)          # set logger level >= INFO
+logger = logging.getLogger(__name__)  # get local logger
+logger.setLevel(logging.DEBUG)  # set logger level >= INFO
 """
 ########################################################################################################################
     - GET SAME FORMATTER INSTANCE FOR ALL HANDLERS
 ########################################################################################################################
 """
-formatter = logging.Formatter('%(asctime)s | %(levelname)-10s | %(name)s | RemovalService.%(funcName)s | %(message)s')  # get formatter
+formatter = logging.Formatter(
+    '%(asctime)s | %(levelname)-10s | %(name)s | RemovalService.%(funcName)s | %(message)s')  # get formatter
 """
 ########################################################################################################################
     - GET CLI HANDLER INSTANCE
@@ -25,27 +26,22 @@ formatter = logging.Formatter('%(asctime)s | %(levelname)-10s | %(name)s | Remov
     - ADD HANDLER TO LOCAL LOGGER
 ########################################################################################################################
 """
-cli_handler = logging.StreamHandler()   # get CLI handler (default=stderr)
-cli_handler.setFormatter(formatter)     # set formatter for CLI handler
-logger.addHandler(cli_handler)          # add CLI handler to logger
+cli_handler = logging.StreamHandler()  # get CLI handler (default=stderr)
+cli_handler.setFormatter(formatter)  # set formatter for CLI handler
+logger.addHandler(cli_handler)  # add CLI handler to logger
 
 
 class RemovalService(object):
     """A service for removing objects from the filesystem."""
 
     def rm(self, filename):
-        # print(f'{__class__.__name__}.{self.rm.__name__}|check file {filename!r}')
         logger.info(f'check file {filename!r}')
         if os.path.isfile(filename):
-            # print(f'{__class__.__name__}.{self.rm.__name__}|{filename!r} exists')
-            # print(f'{__class__.__name__}.{self.rm.__name__}|call os.remove({filename!r})')
             logger.info(f'{filename!r} exists')
             logger.info(f'call os.remove({filename!r})')
-            # os.remove(filename)
+            os.remove(filename)
         else:
-            # print(f'{__class__.__name__}.{self.rm.__name__}|{filename} does not exists')
             logger.error(f'{filename!r} does not exists')
-            pass
 
 
 if __name__ == '__main__':
