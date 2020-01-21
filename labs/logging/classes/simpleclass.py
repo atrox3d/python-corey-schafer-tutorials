91dfb4b3833f78ef6a2573b32f41b9f084f31ba7
atrox3d <atrox3d@gmail.com>
2020-01-21 10:46:11 +0100

    test loggers at various levels

--- labs/logging/classes/simpleclass.py
+++ labs/logging/classes/simpleclass.py
@@ -1,5 +1,25 @@
+from modules import logger
+
+
+"""
+    module level logger
+"""
+modulelogger = logger.getCLIlogger()
+modulelogger.info('hello')
+
+
 class SimpleClass:
-    pass
+    classlogger = logger.getCLIlogger('classlogger')
+    classlogger.info('class')
+
+    def __init__(self, value='default'):
+        self.value = value
+        """
+        instance level logger
+        """
+        self.instancelogger = logger.getCLIlogger('instancelogger')
+        self.instancelogger.info(value)
+        self.classlogger.info(value)
 
 
 if __name__ == '__main__':
