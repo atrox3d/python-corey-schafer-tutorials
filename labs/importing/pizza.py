608b6f970940e9e37dbfae3f796a53e13b6da744
atrox3d <atrox3d@gmail.com>
2020-01-21 14:23:33 +0100

    add pizza.py

---
+++ labs/importing/pizza.py
@@ -0,0 +1,33 @@
+"""
+https://alex.dzyoba.com/blog/python-import/
+"""
+# pizza.py file
+
+import math
+
+
+class Pizza:
+    print('always executed: set fields')
+    name: str = ''
+    size: int = 0
+    price: float = 0
+
+    def __init__(self, name: str, size: int, price: float) -> None:
+        self.name = name
+        self.size = size
+        self.price = price
+
+    def area(self) -> float:
+        return math.pi * math.pow(self.size / 2, 2)
+
+    def awesomeness(self) -> int:
+        if self.name == 'Carbonara':
+            return 9000
+
+        return self.size // int(self.price) * 100
+
+
+print('pizza.py module name is %s' % __name__)
+
+if __name__ == '__main__':
+    print('Carbonara is the most awesome pizza.')
