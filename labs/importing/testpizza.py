b7688a4b798ac00ef1852a7bd09d3d1ed7365b1a
atrox3d <atrox3d@gmail.com>
2020-01-22 11:02:50 +0100

    add testpizza.py

---
+++ labs/importing/testpizza.py
@@ -0,0 +1,10 @@
+import pizzapy                  # works
+from pizzapy import pizza       # works
+
+# from pizzapy import menu
+#       works, but then menu.py cannot find pizza
+#       ModuleNotFoundError: No module named 'pizza'
+
+
+
+
