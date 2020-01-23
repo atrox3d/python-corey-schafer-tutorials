ec215d0279fef8415bfba4bf2e8d4451b6c509cd
atrox3d <atrox3d@gmail.com>
2020-01-23 11:19:51 +0100

    talking inits

--- labs/importing/pizza_app/pizzapy/__init__.py
+++ labs/importing/pizza_app/pizzapy/__init__.py
@@ -1,4 +1,10 @@
 """
 https://alex.dzyoba.com/blog/python-import/
 """
-print(f'__init_.py | hello, {__file__} here')
+from os.path import basename
+thisfile = basename(__file__)
+
+if __name__ == '__main__':
+    print(f'{thisfile} | hello, you are runnig this file with __name__={__name__}')
+else:
+    print(f'{thisfile} | hello, is being executed on import with __name__={__name__}')
