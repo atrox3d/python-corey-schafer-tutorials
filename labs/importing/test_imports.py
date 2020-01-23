7461bcba59b700785520e7b49b22a8b7f779c128
atrox3d <atrox3d@gmail.com>
2020-01-23 10:58:15 +0100

    prettify output and clarify effects of imports and del

--- labs/importing/test_imports.py
+++ labs/importing/test_imports.py
@@ -15,20 +15,30 @@ def filternames(collection, *exclude):
         raise TypeError('need list or dicct')
 
 
+def print_prettifycollection(name, collection, indent=4):
+    print(f'{name}:')
+    if type(collection) is list:
+        for name in collection:
+            print(' ' * indent, name)
+    elif type(collection) is dict:
+        for key, val in collection.items():
+            print(' ' * indent, f'{key:<10}: {val}')
+
+
 def printnames():
     print('-----------------------------------------------------------------------------------------------------------')
-    _locals = filternames(locals(), printnames.__name__)
-    _globals = filternames(globals(), printnames.__name__)
-    _dir = filternames(dir(), printnames.__name__)
+    _locals = filternames(locals(), printnames.__name__, print_prettifycollection.__name__)
+    _globals = filternames(globals(), printnames.__name__, print_prettifycollection.__name__)
+    _dir = filternames(dir(), printnames.__name__, print_prettifycollection.__name__)
 
     if _locals:
-        print('locals : ', _locals)
+        print_prettifycollection('locals : ', _locals)
 
     if _globals:
-        print('globals: ', _globals)
+        print_prettifycollection('globals: ', _globals)
 
     if _dir:
-        print('dir    : ', _dir)
+        print_prettifycollection('dir    : ', _dir)
 
     if not _locals and not _globals and not _dir:
         print("NO VALUES in locals, globals and dir")
@@ -36,23 +46,30 @@ def printnames():
 
 
 printnames()
-
+#
+#
+#
 print('import pizza_app:')
-import pizza_app                                    # useless
+import pizza_app                                    # creates namespace pizza_app
+print(pizza_app)
+# print(pizza_app.pizzapy)                          # AttributeError: module 'pizza_app' has no attribute 'pizzapy'
 # pizza_app.pizzapy.pizza.Pizza('name', 10, 10.0)   # AttributeError: module 'pizza_app' has no attribute 'pizzapy'
 printnames()
 #
 #
 #
 print('import pizza_app.pizzapy:')
-import pizza_app.pizzapy                            # useless
+import pizza_app.pizzapy
 # pizza_app.pizzapy.pizza.Pizza('name', 10, 10.0)   # AttributeError: module 'pizza_app.pizzapy' has no attribute 'pizza'
+print(pizza_app.pizzapy)
 printnames()
 #
 #
 #
 print('from pizza_app import pizzapy:')
-from pizza_app import pizzapy
+from pizza_app import pizzapy                       # creates namespace pizzapy
+print(pizza_app.pizzapy)
+print(pizzapy)
 # pizzapy.pizza.Pizza('', 0, 0)                     # NO
 printnames()
 #
@@ -61,54 +78,86 @@ printnames()
 print('import pizza_app.pizzapy.pizza:')
 import pizza_app.pizzapy.pizza                      #
 # Pizza('name', 10, 10.0)                           # NameError: name 'Pizza' is not defined
+print(pizza_app.pizzapy.pizza)
+# print(pizza)                                      # NameError: name 'pizza' is not defined
+printnames()
+pizza_app.pizzapy.pizza.Pizza('name', 10, 10.0)     # YES
+#
+#
+#
+print('from pizza_app.pizzapy import pizza:')
+from pizza_app.pizzapy import pizza                 # creates namespace pizza
+# Pizza('name', 10, 10.0)                           # NameError: name 'Pizza' is not defined
+print(pizza_app.pizzapy.pizza)
+print(pizza)
 printnames()
 pizza_app.pizzapy.pizza.Pizza('name', 10, 10.0)     # YES
 #
 #
 #
 # import pizza_app.pizzapy.pizza.Pizza              # ModuleNotFoundError: No module named 'pizza_app.pizzapy.pizza.Pizza'; 'pizza_app.pizzapy.pizza' is not a package
+#
+#
+#
 print('from pizza_app.pizzapy.pizza import Pizza:')
-from pizza_app.pizzapy.pizza import Pizza           # YES
+from pizza_app.pizzapy.pizza import Pizza           # creates class Pizza in namespace
+print(pizza_app.pizzapy.pizza)
+print(pizza)
+print(Pizza)
 printnames()
 Pizza('name', 10, 10.0)                             # YES
-"""
-RESET imports
-"""
-#
-#
-#
-print('del Pizza:')
-del Pizza
-printnames()
+print()
+print()
+print()
+print()
+print()
+print('RESET imports')
+print()
+print()
+print()
+print()
+print()
 #
 #
 #
 print('del pizza_app.pizzapy.pizza.Pizza:')
-del pizza_app.pizzapy.pizza.Pizza
+del pizza_app.pizzapy.pizza.Pizza                   # no effect
+printnames()
+#
+#
+#
+print('del Pizza:')
+del Pizza                                           # deletes Pizza
 printnames()
 #
 #
 #
 print('del pizza_app.pizzapy.pizza:')
-del pizza_app.pizzapy.pizza
+del pizza_app.pizzapy.pizza                         # no effect
+printnames()
+#
+#
+#
+print('del pizza:')
+del pizza                                           # removes namespace pizza
 printnames()
 #
 #
 #
 print('del pizza_app.pizzapy:')
-del pizza_app.pizzapy
+del pizza_app.pizzapy                               # no effect
 printnames()
 #
 #
 #
 print('del pizzapy:')
-del pizzapy
+del pizzapy                                         # removes namespace pizzapy
 printnames()
 #
 #
 #
 print('del pizza_app:')
-del pizza_app
+del pizza_app                                       # removes namespace pizza
 printnames()
 
 
