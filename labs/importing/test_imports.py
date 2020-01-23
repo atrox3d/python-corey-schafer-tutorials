0a946dc7bb15ec389c519e829da2c3529528e126
atrox3d <atrox3d@gmail.com>
2020-01-23 10:09:02 +0100

    test changes after del

--- labs/importing/test_imports.py
+++ labs/importing/test_imports.py
@@ -1,24 +1,37 @@
-def filternames(collection, exclude=[]):
+def filternames(collection, *exclude):
     """
     print collection, excluding dunders and names in the optional list
     :param collection:
     :param exclude:
     :return:
     """
+    exclude = list(exclude)
     exclude.append(filternames.__name__)
     if type(collection) is list:
-        return [name for name in collection if not name.startswith('__') and name not in exclude]
+        return [name for name in collection if not name.startswith('_') and name not in exclude]
     elif type(collection) is dict:
-        return {key: val for key, val in collection.items() if not key.startswith('__') and key not in exclude}
+        return {key: val for key, val in collection.items() if not key.startswith('_') and key not in exclude}
     else:
         raise TypeError('need list or dicct')
 
 
 def printnames():
-    print('############################################################################################################')
-    print('locals : ', filternames(locals(), [printnames.__name__]))
-    print('globals: ', filternames(globals(), [printnames.__name__]))
-    print('dir    : ', filternames(dir(), [printnames.__name__]))
+    print('-----------------------------------------------------------------------------------------------------------')
+    _locals = filternames(locals(), printnames.__name__)
+    _globals = filternames(globals(), printnames.__name__)
+    _dir = filternames(dir(), printnames.__name__)
+
+    if _locals:
+        print('locals : ', _locals)
+
+    if _globals:
+        print('globals: ', _globals)
+
+    if _dir:
+        print('dir    : ', _dir)
+
+    if not _locals and not _globals and not _dir:
+        print("NO VALUES in locals, globals and dir")
     print('############################################################################################################')
 
 
@@ -28,39 +41,81 @@ print('import pizza_app:')
 import pizza_app                                    # useless
 # pizza_app.pizzapy.pizza.Pizza('name', 10, 10.0)   # AttributeError: module 'pizza_app' has no attribute 'pizzapy'
 printnames()
-
-print('import pizza_app.pizzapy')
+#
+#
+#
+print('import pizza_app.pizzapy:')
 import pizza_app.pizzapy                            # useless
 # pizza_app.pizzapy.pizza.Pizza('name', 10, 10.0)   # AttributeError: module 'pizza_app.pizzapy' has no attribute 'pizza'
 printnames()
-
-print('import pizza_app.pizzapy.pizza')
+#
+#
+#
+print('from pizza_app import pizzapy:')
+from pizza_app import pizzapy
+# pizzapy.pizza.Pizza('', 0, 0)                     # NO
+printnames()
+#
+#
+#
+print('import pizza_app.pizzapy.pizza:')
 import pizza_app.pizzapy.pizza                      #
 # Pizza('name', 10, 10.0)                           # NameError: name 'Pizza' is not defined
 printnames()
 pizza_app.pizzapy.pizza.Pizza('name', 10, 10.0)     # YES
-
+#
+#
+#
 # import pizza_app.pizzapy.pizza.Pizza              # ModuleNotFoundError: No module named 'pizza_app.pizzapy.pizza.Pizza'; 'pizza_app.pizzapy.pizza' is not a package
-print('from pizza_app.pizzapy.pizza import Pizza')
+print('from pizza_app.pizzapy.pizza import Pizza:')
 from pizza_app.pizzapy.pizza import Pizza           # YES
 printnames()
 Pizza('name', 10, 10.0)                             # YES
 """
 RESET imports
 """
-# del Pizza
-# del pizza_app.pizzapy.pizza.Pizza
-# del pizza_app.pizzapy.pizza
-# del pizza_app.pizzapy
-# del pizza_app
+#
+#
+#
+print('del Pizza:')
+del Pizza
+printnames()
+#
+#
+#
+print('del pizza_app.pizzapy.pizza.Pizza:')
+del pizza_app.pizzapy.pizza.Pizza
+printnames()
+#
+#
+#
+print('del pizza_app.pizzapy.pizza:')
+del pizza_app.pizzapy.pizza
+printnames()
+#
+#
+#
+print('del pizza_app.pizzapy:')
+del pizza_app.pizzapy
+printnames()
+#
+#
+#
+print('del pizzapy:')
+del pizzapy
+printnames()
+#
+#
+#
+print('del pizza_app:')
+del pizza_app
+printnames()
 
-# print('locals : ', locals())
-# print('globals: ', globals())
-# print('dir    : ', dir())
-# from pizza_app.pizzapy.pizza import Pizza
-# print('locals : ', locals())
-# print('globals: ', globals())
-# print('dir    : ', dir())
+
+
+
+
+
 
 
 
