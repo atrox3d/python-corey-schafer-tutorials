714920bbf68ead55bb53b5a072d50b3d8bf78ce2
atrox3d <atrox3d@gmail.com>
2020-01-23 08:17:50 +0100

    add reference to stackoverflow and exerpt from post

--- labs/importing/pizza_app/pizzapy/menu.py
+++ labs/importing/pizza_app/pizzapy/menu.py
@@ -1,15 +1,19 @@
 """
 https://alex.dzyoba.com/blog/python-import/
 """
-"""
-https://stackoverflow.com/questions/45446418/modulenotfounderror-no-module-named-main-xxxx-main-is-not-a-packag
-"""
 # menu.py file
 
 from typing import List
+
 # from pizza import Pizza                   # not working
 # from pizzapy.pizza import Pizza           # working version
-from pizza_app.pizzapy.pizza import Pizza   # most correct version
+"""
+https://stackoverflow.com/questions/45446418/modulenotfounderror-no-module-named-main-xxxx-main-is-not-a-packag
+
+Note that relative imports are based on the name of the current module. Since the name of the main module is always 
+__main__, modules intended for use as the main module of a Python application must always use absolute imports. 
+"""
+from pizza_app.pizzapy.pizza import Pizza  # most correct version
 
 MENU: List[Pizza] = [
     Pizza('Margherita', 30, 10),
@@ -19,4 +23,3 @@ MENU: List[Pizza] = [
 
 if __name__ == '__main__':
     print('menu.py    | ', MENU)
-
