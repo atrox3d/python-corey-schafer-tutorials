7c8b4fc12f6a4092867a8b1461b68cdcf6165e26
atrox3d <atrox3d@gmail.com>
2020-01-27 08:59:32 +0100

    https://alex.dzyoba.com/blog/python-import : add menu.py

---
+++ labs/importing/pizzaapp/pizzapy/menu.py
@@ -0,0 +1,13 @@
+# menu.py file
+
+from typing import List
+from pizza import Pizza
+
+MENU: List[Pizza] = [
+    Pizza('Margherita', 30, 10.0),
+    Pizza('Carbonara', 45, 14.99),
+    Pizza('Marinara', 35, 16.99),
+]
+
+if __name__ == '__main__':
+    print(MENU)
