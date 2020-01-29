dfe2de05788521d5389fcc384f78a5e20b7c9f0d
atrox3d <atrox3d@gmail.com>
2020-01-29 10:20:31 +0100

    remove experiments

--- labs/importing/pizzaapp/pizzapy/__init__.py
+++ labs/importing/pizzaapp/pizzapy/__init__.py
@@ -1,24 +1,24 @@
 # pizzapy/__init__.py
 print('pizzapy/__init__.py')
 
-EXPLICIT_RELATIVE_IMPORT = False
-
-print(f'EXPLICIT_RELATIVE_IMPORT is {EXPLICIT_RELATIVE_IMPORT}')
-
-if EXPLICIT_RELATIVE_IMPORT:
-    print('from pizzapy.pizza import *')
-    from pizzapy.pizza import *
-    print('ok')
-
-    print('from pizzapy.menu import *')
-    from pizzapy.menu import *
-    print('ok')
-else:
-    print('from .pizza import *')
-    from .pizza import *
-    print('ok')
-
-    print('from .menu import *')
-    from .menu import *
-    print('ok')
-
+# EXPLICIT_RELATIVE_IMPORT = False
+#
+# print(f'EXPLICIT_RELATIVE_IMPORT is {EXPLICIT_RELATIVE_IMPORT}')
+#
+# if EXPLICIT_RELATIVE_IMPORT:
+#     print('from pizzapy.pizza import *')
+#     from pizzapy.pizza import *
+#     print('ok')
+#
+#     print('from pizzapy.menu import *')
+#     from pizzapy.menu import *
+#     print('ok')
+# else:
+#     print('from .pizza import *')
+#     from .pizza import *
+#     print('ok')
+#
+#     print('from .menu import *')
+#     from .menu import *
+#     print('ok')
+#
