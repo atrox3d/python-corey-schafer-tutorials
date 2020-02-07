40d9e631d322533bc72521c40a6a931470cec8a6
atrox3d <atrox3d@gmail.com>
2020-02-07 07:39:26 +0100

    add url to each script

---
+++ labs/test/packA/subA/__init__.py
@@ -0,0 +1,3 @@
+"""
+https://chrisyeh96.github.io/2017/08/08/definitive-guide-python-imports.html
+"""
--- labs/test/packA/subA/sa2.py
+++ labs/test/packA/subA/sa2.py
@@ -1,3 +1,6 @@
+"""
+https://chrisyeh96.github.io/2017/08/08/definitive-guide-python-imports.html
+"""
 import os
 import os.path
 
