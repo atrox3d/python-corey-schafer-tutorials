8143999351caef3b67f0037b8b1c0266b3e8cd88
atrox3d <atrox3d@gmail.com>
2021-01-30 08:50:20 +0100

    add youtube title, add output in comments

--- 01-python-basics/08-functions.py
+++ 01-python-basics/08-functions.py
@@ -1,44 +1,51 @@
 #
 # https://www.youtube.com/watch?v=9Os0o3wzS_I
 #
+# Python Tutorial for Beginners 8: Functions
+#
 #################################################################################
-
-
+#
+#
+#
 def hello_func():
     """simple hello function"""
     print('hello from hello_func')
 
 
-print(hello_func)
-hello_func()
-
-
+print(hello_func)                   # <function hello_func at 0x0034AAD8>
+hello_func()                        # hello from hello_func
+#
+#
+#
 def hello_func():
     """function returning string"""
     return 'hello from hello_func'
 
 
-print(hello_func())
-print(hello_func().upper())
-
-
+print(hello_func())                 # hello from hello_func
+print(hello_func().upper())         # HELLO FROM HELLO_FUNC
+#
+#
+#
 def hello_func(greeting):
     """function returns strings and accept parameter"""
     return '{} Function.'.format(greeting)
 
 
-print(hello_func('hello'))
-
-
+print(hello_func('hello'))          # hello Function.
+#
+#
+#
 def hello_func(greeting, name='you'):
     """function with positional argument and optional parameter with default value"""
     return '{}, {}.'.format(greeting, name)
 
 
-print(hello_func('hello'))
-print(hello_func('hello', 'robb'))
-
-
+print(hello_func('hello'))          # hello, you.
+print(hello_func('hello', 'robb'))  # hello, robb.
+#
+#
+#
 def student_info(*args, **kwargs):
     """function with optional positional arguments and optional keyword arguments"""
     print('\nstudent_info')
@@ -47,15 +54,27 @@ def student_info(*args, **kwargs):
 
 
 student_info('args1', 'args2', kwargs1=1, kwargs2=2)
+                                    # student_info
+                                    # args   : ('args1', 'args2')
+                                    # kwargs : {'kwargs1': 1, 'kwargs2': 2}
 #
 #   unpacking parameters
 #
 courses = ['math', 'art']
 info = {'name': 'robb', 'age': 40}
+#
 # not what we want:
 # courses and info become two elements of the tuple args
+#
 student_info(courses, info)
-
-# correct
+                                    # student_info
+                                    # args   : (['math', 'art'], {'name': 'robb', 'age': 40})
+                                    # kwargs : {}
+#
+# correct, unpacking
+#
 student_info(*courses, **info)
+                                    # student_info
+                                    # args   : ('math', 'art')
+                                    # kwargs : {'name': 'robb', 'age': 40}
 
