0b3a681597c565aabe376f7cb6f74743201f0908
atrox3d <atrox3d@gmail.com>
2021-01-30 23:48:24 +0100

    add youtube title, fix output

--- 01-python-basics/09-imports.py
+++ 01-python-basics/09-imports.py
@@ -1,39 +1,55 @@
 #
 # https://www.youtube.com/watch?v=CqvZ3vGoGs0
 #
+# Python Tutorial for Beginners 9: Import Modules and Exploring The Standard Library
+#
 #################################################################################
+#
 # normal import
-from modules import module, module as m
 # rename module
+#
+from modules import module, module as m
+#
 # import some symbol
 # import and rename symbols
+#
 from modules.module import find_index as fi
+#
 # import everything
+#
 from modules.module import *
 
 import sys
 
 courses = ['history', 'math', 'physics', 'compsci']
-
+print(f"courses = {courses}")
+#
 # fully qualified name module
+#
 index = module.find_index(courses, 'math')
-print(index)
-
+print(f'math {"found at " + str(index) if index >= 0 else "not found"}')
+#
 # renamed module
+#
 index = m.find_index(courses, 'math')
-print(index)
-
+print(f'math {"found at " + str(index) if index >= 0 else "not found"}')
+#
 # just the function
+#
 index = find_index(courses, 'math')
-print(index)
-
+print(f'math {"found at " + str(index) if index >= 0 else "not found"}')
+#
 # renamed function
+#
 index = fi(courses, 'math')
-print(index)
-
+print(f'math {"found at " + str(index) if index >= 0 else "not found"}')
+#
+#   print module symbol
+#
 print(test)
 
 print(f'sys.path = {sys.path}')
+
 for path in sys.path:
     print(f'path = {path}')
 
--- modules/module.py
+++ modules/module.py
@@ -2,17 +2,26 @@
 # https://www.youtube.com/watch?v=CqvZ3vGoGs0
 #
 #################################################################################
-print('Imported my_module...')
+print('Imported my_module.py')
+print(f'Imported {__file__}')
 
 test = 'Test String'
 
 
 def find_index(to_search, target):
     """Find the index of a value in a sequence"""
+    #
+    #   associate index to every element
+    #
     for i, value in enumerate(to_search):
+        #
+        #   return index if value is found
+        #
         if value == target:
             return i
-
+    #
+    #   return -1 if value not found
+    #
     return -1
 
 
